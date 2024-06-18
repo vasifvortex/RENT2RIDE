291d1e1716692ea350261b4bac2443b7321b7329
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-06-18 15:50:22 +0400

    Delete rent2ride directory

--- rent2ride/app/static/js/url_helpers.js
+++
@@ -1,8 +0,0 @@
-export const GET_ALL_BIKES = "/api/read/"
-export const GET_SINGLE_BIKE = "/api/read_single/<int:cycle_id>"
-export const CREATE_BIKE = "/api/creat" 
-export const UPDATE_BIKE = "/api/update/<int:cycle_id>"
-export const DELETE_BIKE = "/api/delete/<int:cycle_id>"
-
-
-
